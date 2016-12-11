974773684ad578fd09006d76db57120242d18ce4
Gael Arrambide <gael_arrambide@hotmail.com>
2016-12-10 21:09:06 -0600

    server change

--- server.js
+++ server.js
@@ -161,6 +161,6 @@ app.post("/articles/:id", function(req, res) {
 
 
 // Listen on port 3000
-app.listen(3000, function() {
-  console.log("App running on port 3000!");
+app.listen(process.env.PORT || 3000, function(){
+  console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
 });
